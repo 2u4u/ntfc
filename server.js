f50f29dbcd49ed03081d5a40bd5b52d91048fd81
2u4u <2u4u.dev@gmail.com>
2023-03-26 20:13:19 +0400

    add cron

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const botToken = process.env.TELEGRAM_BOT_TOKEN;
+const targetChatId = process.env.TELEGRAM_CHAT_ID;
+
 const app = express();
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+const bot = new Telegraf(botToken);
 
 // Start the bot
 bot.launch();
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
+
 
 app.post('/api/send-message', async (req, res) => {
   try {
@@ -23,13 +27,24 @@ app.post('/api/send-message', async (req, res) => {
       return;
     }
 
-    await bot.telegram.sendMessage(process.env.TELEGRAM_CHAT_ID, message);
+    await bot.telegram.sendMessage(targetChatId, message);
 
     res.status(200).json({ success: true, message: 'Message sent to Telegram bot' });
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Failed to send message to Telegram bot' });
   }
+});
+
+// Schedule a notification at an exact time
+// The cron format is 'minute hour day_of_month month day_of_week'
+// For example, to send notifications at 9:00 AM and 9:00 PM, use '0 9,21 * * *'
+const notificationCronExpression = '0 9,21 * * *';
+cron.schedule(notificationCronExpression, () => {
+  bot.telegram.sendMessage(targetChatId, 'I am still alive!')
+    .catch((error) => {
+      console.error('Failed to send alive notification:', error);
+    });
 });
 
 const PORT = process.env.PORT || 3000;
